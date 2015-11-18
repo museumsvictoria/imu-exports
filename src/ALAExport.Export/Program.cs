b6ade9ccff7abd77b544df3178d5bb56b5e33927
michael-mason <cptshiner@gmail.com>
2015-11-18 16:07:08 +1100

    added command line parser

--- src/ALAExport.Export/Program.cs
+++ src/ALAExport.Export/Program.cs
@@ -10,6 +10,10 @@ namespace ALAExport.Export
 
         static void Main(string[] args)
         {
+            // Parse command line options
+            CommandLineConfig.Initialize(args);
+
+            // Configure serilog
             SerilogConfig.Initialize();
 
             // Set up Ctrl+C handling 
@@ -24,8 +28,10 @@ namespace ALAExport.Export
             // Log any exceptions that are not handled
             AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) => Log.Logger.Fatal((Exception)eventArgs.ExceptionObject, "Unhandled Exception occured in export");
             
+            // Wire up ioc
             var container = ContainerConfig.Initialize();
 
+            // Begin export
             container.GetInstance<TaskRunner>().RunAllTasks();
         }
     }
