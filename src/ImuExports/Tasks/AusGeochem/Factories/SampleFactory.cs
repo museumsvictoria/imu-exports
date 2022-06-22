b40f7cdb39d4901f786f570c4573916e5e69119d
michael-mason <cptshiner@gmail.com>
2022-06-22 15:23:25 +1000

    update ausgeochem mapping
    - updated lithology comment and location name

--- src/ImuExports/Tasks/AusGeochem/Factories/SampleFactory.cs
+++ src/ImuExports/Tasks/AusGeochem/Factories/SampleFactory.cs
@@ -21,7 +21,6 @@ public class SampleFactory : IFactory<Sample>
         }.Concatenate(" ");
 
         if (map.GetTrimStrings("ColCollectionName_tab").Any())
-        {
             sample.ArchiveNotes = map.GetMaps("prevno")
                 .Where(x => !string.IsNullOrWhiteSpace(x.GetTrimString("ManPreviousCollectionName_tab")))
                 .Select(x =>
@@ -31,8 +30,7 @@ public class SampleFactory : IFactory<Sample>
                         x.GetTrimString("ManPreviousNumbers_tab")
                     }.Concatenate(": ")
                 ).Concatenate(" | ");
-        }
-        
+
         var site = map.GetMap("site");
         if (site != null)
         {
@@ -51,17 +49,17 @@ public class SampleFactory : IFactory<Sample>
                     sample.Longitude = decimalLongitude.FirstOrDefault(x => x != null)?.ToString();
 
                 sample.LatLongPrecision = latlong.GetTrimString("LatRadiusNumeric_tab");
-                
+
                 if (DateTime.TryParseExact(latlong.GetTrimString("LatDetDate0"), "dd/MM/yyyy",
                         new CultureInfo("en-AU"), DateTimeStyles.None, out var dateGeoreferenced))
                     sample.DateGeoreferenced = dateGeoreferenced.ToString("yyyy-MM-dd");
-                
+
                 georeferenceAssignedNotes = new[]
                 {
                     MakePartyName(latlong.GetMap("determinedBy")),
                     latlong.GetTrimString("LatDetDate0")
                 }.Concatenate(", ");
-                
+
                 latlongLocationNotes = new[]
                 {
                     sample.Latitude,
@@ -71,6 +69,8 @@ public class SampleFactory : IFactory<Sample>
                         : latlong.GetTrimString("LatDatum_tab")
                 }.Concatenate(", ");
             }
+
+            sample.LocationName = site.GetTrimString("LocPreciseLocation");
 
             var geo = site.GetMaps("geo").FirstOrDefault();
             string geoLocationNotes = null;
@@ -85,16 +85,8 @@ public class SampleFactory : IFactory<Sample>
                     geo.GetTrimString("LocNearestNamedPlace_tab"),
                     geo.GetTrimString("LocPreciseLocation")
                 }.Concatenate(", ");
-
-                sample.LocationName = new[]
-                {
-                    geo.GetTrimString("LocDistrictCountyShire_tab"),
-                    geo.GetTrimString("LocTownship_tab"),
-                    geo.GetTrimString("LocNearestNamedPlace_tab"),
-                    geo.GetTrimString("LocPreciseLocation")
-                }.Concatenate(", ");
             }
-            
+
             sample.LocationNotes = new[]
             {
                 latlongLocationNotes,
@@ -103,7 +95,7 @@ public class SampleFactory : IFactory<Sample>
                     : $"Georeference assigned: {georeferenceAssignedNotes}",
                 geoLocationNotes
             }.Concatenate(" | ");
-            
+
             sample.UnitName = site.GetTrimStrings("EraMvRockUnit_tab").Concatenate(", ");
             sample.UnitAge = new[]
             {
@@ -126,89 +118,94 @@ public class SampleFactory : IFactory<Sample>
 
             sample.DepthMin = site.GetTrimString("EraDepthFromMt");
             sample.DepthMax = site.GetTrimString("EraDepthToMt");
-            
-            sample.Collector = map.GetMaps("collectors").Select(MakePartyName).Concatenate(", ");
-
-            if (!string.IsNullOrWhiteSpace(sample.Collector))
-                sample.PersonRole = "Collector";
-            
-            if (DateTime.TryParseExact(map.GetTrimString("LocDateCollectedFrom"), "dd/MM/yyyy",
-                    new CultureInfo("en-AU"), DateTimeStyles.None, out var dateCollectedMin))
-                sample.DateCollectedMin = dateCollectedMin.ToString("yyyy-MM-dd");
-            
-            if (DateTime.TryParseExact(map.GetTrimString("LocDateCollectedTo"), "dd/MM/yyyy",
-                    new CultureInfo("en-AU"), DateTimeStyles.None, out var dateCollectedMax))
-                sample.DateCollectedMax = dateCollectedMax.ToString("yyyy-MM-dd");
-
-            if (map.GetTrimString("ColDiscipline") == "Mineralogy")
-                sample.SampleKind = "Mineral";
-            if (map.GetTrimString("ColDiscipline") == "Petrology")
-                sample.SampleKind = "Rock";
-
-            sample.SpecimenState = map.GetMaps("preparations")
-                .Select(x => new[]
-                {
-                    x.GetString("StrSpecimenForm_tab")
-                })
-                .SelectMany(x => x)
-                .Distinct()
-                .Concatenate(",");
-
-            if (map.GetTrimString("ColDiscipline") == "Mineralogy")
-            {
-                sample.MineralId = map.GetTrimString("MinSpecies");
-                
-                string typeSpecimen = null;
-                if (map.GetTrimString("MinType") == "Yes")
-                    typeSpecimen = map.GetTrimString("MinTypeType");
-                
-                sample.Comment = new[]
-                {
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinSpecies"))
-                        ? null
-                        : $"Mineral Species: {map.GetTrimString("MinSpecies")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinVariety"))
-                        ? null
-                        : $"Mineral Variety: {map.GetTrimString("MinVariety")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinAssociatedMatrix"))
-                        ? null
-                        : $"Associated Matrix: {map.GetTrimString("MinAssociatedMatrix")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinXrayed")) || map.GetTrimString("MinXrayed") != "Yes"
-                        ? null
-                        : $"X-rayed: {map.GetTrimString("MinXrayed")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinChemicalAnalysis")) ||
-                    map.GetTrimString("MinChemicalAnalysis") != "Yes"
-                        ? null
-                        : $"Chemical Analysis: {map.GetTrimString("MinChemicalAnalysis")}",
-                    typeSpecimen == null ? null : $"Type specimen: {typeSpecimen}"
-                }.Concatenate(" | ");
-            }
-
-            if (map.GetTrimString("ColDiscipline") == "Petrology")
-            {
-                sample.MineralId = map.GetTrimString("RocRockName");
-
-                sample.Comment = new[]
-                {
-                    string.IsNullOrWhiteSpace(map.GetTrimString("RocRockDescription"))
-                        ? null
-                        : $"Description: {map.GetTrimString("RocRockDescription")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("RocMainMineralsPresent"))
-                        ? null
-                        : $"Main Minerals: {map.GetTrimString("RocMainMineralsPresent")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("RocThinSection")) ||
-                    map.GetTrimString("RocThinSection") != "Yes"
-                        ? null
-                        : $"Thin Section: {map.GetTrimString("RocThinSection")}",
-                    string.IsNullOrWhiteSpace(map.GetTrimString("MinChemicalAnalysis")) ||
-                    map.GetTrimString("MinChemicalAnalysis") != "Yes"
-                        ? null
-                        : $"Chemical Analysis: {map.GetTrimString("MinChemicalAnalysis")}"
-                }.Concatenate(" | ");
-            }
-
-            sample.LastKnownLocation = "Museums Victoria";
         }
+
+        sample.Collector = map.GetMaps("collectors").Select(MakePartyName).Concatenate(", ");
+
+        if (!string.IsNullOrWhiteSpace(sample.Collector))
+            sample.PersonRole = "Collector";
+
+        if (DateTime.TryParseExact(map.GetTrimString("LocDateCollectedFrom"), "dd/MM/yyyy",
+                new CultureInfo("en-AU"), DateTimeStyles.None, out var dateCollectedMin))
+            sample.DateCollectedMin = dateCollectedMin.ToString("yyyy-MM-dd");
+
+        if (DateTime.TryParseExact(map.GetTrimString("LocDateCollectedTo"), "dd/MM/yyyy",
+                new CultureInfo("en-AU"), DateTimeStyles.None, out var dateCollectedMax))
+            sample.DateCollectedMax = dateCollectedMax.ToString("yyyy-MM-dd");
+
+        if (map.GetTrimString("ColDiscipline") == "Mineralogy")
+            sample.SampleKind = "Mineral";
+        if (map.GetTrimString("ColDiscipline") == "Petrology")
+            sample.SampleKind = "Rock";
+
+        sample.SpecimenState = map.GetMaps("preparations")
+            .Select(x => new[]
+            {
+                x.GetString("StrSpecimenForm_tab")
+            })
+            .SelectMany(x => x)
+            .Distinct()
+            .Concatenate(",");
+        
+        // Mineralogy specific
+        if (map.GetTrimString("ColDiscipline") == "Mineralogy")
+        {
+            sample.MineralId = map.GetTrimString("MinSpecies");
+
+            string typeSpecimen = null;
+            if (map.GetTrimString("MinType") == "Yes")
+                typeSpecimen = map.GetTrimString("MinTypeType");
+
+            sample.Comment = new[]
+            {
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinSpecies"))
+                    ? null
+                    : $"Mineral Species: {map.GetTrimString("MinSpecies")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinVariety"))
+                    ? null
+                    : $"Mineral Variety: {map.GetTrimString("MinVariety")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinAssociatedMatrix"))
+                    ? null
+                    : $"Associated Matrix: {map.GetTrimString("MinAssociatedMatrix")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinXrayed")) || map.GetTrimString("MinXrayed") != "Yes"
+                    ? null
+                    : $"X-rayed: {map.GetTrimString("MinXrayed")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinChemicalAnalysis")) ||
+                map.GetTrimString("MinChemicalAnalysis") != "Yes"
+                    ? null
+                    : $"Chemical Analysis: {map.GetTrimString("MinChemicalAnalysis")}",
+                typeSpecimen == null ? null : $"Type specimen: {typeSpecimen}"
+            }.Concatenate(" | ");
+        }
+
+        // Petrology specific
+        if (map.GetTrimString("ColDiscipline") == "Petrology")
+        {
+            sample.MineralId = map.GetTrimString("RocRockName");
+
+            sample.Comment = new[]
+            {
+                string.IsNullOrWhiteSpace(map.GetTrimString("RocRockName"))
+                    ? null
+                    : $"Name: {map.GetTrimString("RocRockName")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("RocRockDescription"))
+                    ? null
+                    : $"Description: {map.GetTrimString("RocRockDescription")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("RocMainMineralsPresent"))
+                    ? null
+                    : $"Main Minerals: {map.GetTrimString("RocMainMineralsPresent")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("RocThinSection")) ||
+                map.GetTrimString("RocThinSection") != "Yes"
+                    ? null
+                    : $"Thin Section: {map.GetTrimString("RocThinSection")}",
+                string.IsNullOrWhiteSpace(map.GetTrimString("MinChemicalAnalysis")) ||
+                map.GetTrimString("MinChemicalAnalysis") != "Yes"
+                    ? null
+                    : $"Chemical Analysis: {map.GetTrimString("MinChemicalAnalysis")}"
+            }.Concatenate(" | ");
+        }
+
+        sample.LastKnownLocation = "Museums Victoria";
 
         return sample;
     }
@@ -217,7 +214,7 @@ public class SampleFactory : IFactory<Sample>
     {
         return maps.Select(map => Make(map, stoppingToken));
     }
-    
+
     private string MakePartyName(Map map)
     {
         if (map == null) return null;
